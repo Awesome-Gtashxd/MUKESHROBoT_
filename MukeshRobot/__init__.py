cd62f51351bb837038fa3c5e2b302e814a503f80
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-22 21:40:44 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -57,7 +57,7 @@ if ENV:
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     WORKERS = int(os.environ.get("WORKERS", 8))
-
+    DURATION_LIMIT = int(os.environ.get("DURATION_LIMIT", "90"))
     try:
         OWNER_ID = int(os.environ.get("OWNER_ID", None))
     except ValueError:
