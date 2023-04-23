47d4cc534d5ece45f0949140497f551fbd8fce20
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-23 07:04:23 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -1001,7 +1001,7 @@ if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
     pbot.start()
-    main()
+    await main()
     asyncio.get_event_loop().run_until_complete(mukesh_startup())
     LOGGER.error("Mukesh Music Bot Stopped.")
     pytgcalls.start()
